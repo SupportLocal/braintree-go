700c6cc200c0fdebb797daa72b801de815285f0f
Cory LaNou <cory@joinlocal.com>
2013-11-24 07:50:49 -0700

    All credit cards do end to end itegration testing
    If no recordings are present, all credit card tests
    now do end to end integration and save the recordings
    for future tests.
    
    Same issue of clearing sandbox data applies here...
    
    	modified:   credit_card_test.go

--- credit_card_test.go
+++ credit_card_test.go
@@ -1,6 +1,7 @@
 package braintree
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -17,7 +18,7 @@ var testCreditCard = CreditCard{
 
 func testCreditCardCreate(credit_card *CreditCard) error {
 	server := newServer(func(w http.ResponseWriter, r *http.Request) {
-		if err := serveRecording(w, r, "credit_card", "create_test_credit_card_"+credit_card.Number, http.StatusCreated); err != nil {
+		if err := serveRecording(w, r, "credit_card", fmt.Sprintf("create_test_credit_card_%s_%s", credit_card.CustomerId, credit_card.Number), http.StatusCreated); err != nil {
 			panic(err)
 		}
 	})
@@ -135,7 +136,7 @@ func TestCreditCardUpdate(t *testing.T) {
 
 func TestCreditCardDelete(t *testing.T) {
 	customer := testCustomer
-	customer.Id = "test_customer_credit_card_delete"
+	customer.Id = "cc_credit_card_delete"
 	_ = testCustomerDelete(customer.Id)
 	_ = testCustomerCreate(&customer)
 	testCard := CreditCard{
@@ -173,39 +174,15 @@ func TestCreditCardDelete(t *testing.T) {
 }
 
 func TestCreateCreditCardWithExpirationMonthAndYear(t *testing.T) {
-	var response = []byte(`<?xml version="1.0" encoding="UTF-8"?>
-<credit-card>
-  <bin>411111</bin>
-  <card-type>Visa</card-type>
-  <cardholder-name nil="true"/>
-  <commercial>Unknown</commercial>
-  <country-of-issuance>Unknown</country-of-issuance>
-  <created-at type="datetime">2013-11-20T23:05:08Z</created-at>
-  <customer-id>57131083</customer-id>
-  <customer-location>US</customer-location>
-  <debit>Unknown</debit>
-  <default type="boolean">true</default>
-  <durbin-regulated>Unknown</durbin-regulated>
-  <expiration-month>05</expiration-month>
-  <expiration-year>2014</expiration-year>
-  <expired type="boolean">false</expired>
-  <healthcare>Unknown</healthcare>
-  <image-url>https://assets.braintreegateway.com/payment_method_logo/visa.png?environment=sandbox&amp;merchant_id=foo</image-url>
-  <issuing-bank>Unknown</issuing-bank>
-  <last-4>1111</last-4>
-  <payroll>Unknown</payroll>
-  <prepaid>Unknown</prepaid>
-  <subscriptions type="array"/>
-  <token>gr75sr</token>
-  <unique-number-identifier>0d4987f6852ffa8612463aff627d84b5</unique-number-identifier>
-  <updated-at type="datetime">2013-11-20T23:05:08Z</updated-at>
-  <venmo-sdk type="boolean">false</venmo-sdk>
-  <verifications type="array"/>
-</credit-card>`)
+	customer := testCustomer
+	customer.Id = "cc_with_exp_m_v"
+	_ = testCustomerDelete(customer.Id)
+	_ = testCustomerCreate(&customer)
 
 	server := newServer(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusCreated)
-		writeZip(w, response)
+		if err := serveRecording(w, r, "credit_card", "create_with_exp_m_v", http.StatusOK); err != nil {
+			panic(err)
+		}
 	})
 	defer server.Close()
 
@@ -214,7 +191,7 @@ func TestCreateCreditCardWithExpirationMonthAndYear(t *testing.T) {
 	g := gw.CreditCard()
 
 	card := CreditCard{
-		CustomerId:      "57131083",
+		CustomerId:      customer.Id,
 		Number:          testCreditCards["visa"].Number,
 		ExpirationMonth: "05",
 		ExpirationYear:  "2014",
@@ -228,38 +205,15 @@ func TestCreateCreditCardWithExpirationMonthAndYear(t *testing.T) {
 	if card.Token == "" {
 		t.Fatal("invalid token")
 	}
+
+	_ = testCustomerDelete(customer.Id)
 }
 
 func TestCreateCreditCardInvalidInput(t *testing.T) {
-	var response = []byte(`<?xml version="1.0" encoding="UTF-8"?>
-<api-error-response>
-  <errors>
-    <errors type="array"/>
-    <credit-card>
-      <errors type="array">
-        <error>
-          <code>91704</code>
-          <attribute type="symbol">customer_id</attribute>
-          <message>Customer ID is required.</message>
-        </error>
-      </errors>
-    </credit-card>
-  </errors>
-  <params>
-    <credit-card>
-      <expiration-date>05/14</expiration-date>
-    </credit-card>
-    <action>create</action>
-    <controller>payment_methods</controller>
-    <merchant-id>foo</merchant-id>
-  </params>
-  <message>Customer ID is required.</message>
-</api-error-response>`)
-
 	server := newServer(func(w http.ResponseWriter, r *http.Request) {
-		// This seems like it shouldn't be a status created repsonse if we get an error...  Is this an API bug?
-		w.WriteHeader(http.StatusCreated)
-		writeZip(w, response)
+		if err := serveRecording(w, r, "credit_card", "create_with_inv_exp_m_v", http.StatusOK); err != nil {
+			panic(err)
+		}
 	})
 	defer server.Close()
 
@@ -282,87 +236,24 @@ func TestCreateCreditCardInvalidInput(t *testing.T) {
 	if card.ErrorCount() != 1 {
 		t.Fatal(card.ErrorCount(), "Error count should be 1")
 	}
-	// TODO: validate fields
+	expectedError := "Customer ID is required."
+	if card.ErrorMessage != expectedError {
+		t.Fatal(fmt.Sprintf("Error should be %q, got %q", expectedError, card.ErrorMessage))
+	}
 }
 
 func TestFindCreditCard(t *testing.T) {
-	var response = []byte(`<?xml version="1.0" encoding="UTF-8"?>
-<credit-card>
-  <bin>411111</bin>
-  <card-type>Visa</card-type>
-  <cardholder-name nil="true"/>
-  <commercial>Unknown</commercial>
-  <country-of-issuance>Unknown</country-of-issuance>
-  <created-at type="datetime">2013-11-20T23:05:15Z</created-at>
-  <customer-id>74987569</customer-id>
-  <customer-location>US</customer-location>
-  <debit>Unknown</debit>
-  <default type="boolean">true</default>
-  <durbin-regulated>Unknown</durbin-regulated>
-  <expiration-month>05</expiration-month>
-  <expiration-year>2014</expiration-year>
-  <expired type="boolean">false</expired>
-  <healthcare>Unknown</healthcare>
-  <image-url>https://assets.braintreegateway.com/payment_method_logo/visa.png?environment=sandbox&amp;merchant_id=foo</image-url>
-  <issuing-bank>Unknown</issuing-bank>
-  <last-4>1111</last-4>
-  <payroll>Unknown</payroll>
-  <prepaid>Unknown</prepaid>
-  <subscriptions type="array"/>
-  <token>ghd3x6</token>
-  <unique-number-identifier>0d4987f6852ffa8612463aff627d84b5</unique-number-identifier>
-  <updated-at type="datetime">2013-11-20T23:05:14Z</updated-at>
-  <venmo-sdk type="boolean">false</venmo-sdk>
-  <verifications type="array">
-    <verification>
-      <status>verified</status>
-      <cvv-response-code>M</cvv-response-code>
-      <avs-error-response-code nil="true"/>
-      <avs-postal-code-response-code>I</avs-postal-code-response-code>
-      <avs-street-address-response-code>I</avs-street-address-response-code>
-      <gateway-rejection-reason nil="true"/>
-      <merchant-account-id>foo</merchant-account-id>
-      <processor-response-code>1000</processor-response-code>
-      <processor-response-text>Approved</processor-response-text>
-      <id>557nbb</id>
-      <billing>
-        <first-name nil="true"/>
-        <last-name nil="true"/>
-        <company nil="true"/>
-        <street-address nil="true"/>
-        <extended-address nil="true"/>
-        <locality nil="true"/>
-        <region nil="true"/>
-        <postal-code nil="true"/>
-        <country-name nil="true"/>
-      </billing>
-      <credit-card>
-        <token>ghd3x6</token>
-        <bin>411111</bin>
-        <last-4>1111</last-4>
-        <card-type>Visa</card-type>
-        <expiration-month>05</expiration-month>
-        <expiration-year>2014</expiration-year>
-        <customer-location>US</customer-location>
-        <cardholder-name nil="true"/>
-        <prepaid>Unknown</prepaid>
-        <healthcare>Unknown</healthcare>
-        <debit>Unknown</debit>
-        <durbin-regulated>Unknown</durbin-regulated>
-        <commercial>Unknown</commercial>
-        <payroll>Unknown</payroll>
-        <issuing-bank>Unknown</issuing-bank>
-        <country-of-issuance>Unknown</country-of-issuance>
-      </credit-card>
-      <created-at type="datetime">2013-11-20T23:05:14Z</created-at>
-      <updated-at type="datetime">2013-11-20T23:05:15Z</updated-at>
-    </verification>
-  </verifications>
-</credit-card>`)
+	customer := testCustomer
+	customer.Id = "cc_find"
+	_ = testCustomerCreate(&customer)
+	testCard := testCreditCard
+	testCard.CustomerId = customer.Id
+	_ = testCreditCardCreate(&testCard)
 
 	server := newServer(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		writeZip(w, response)
+		if err := serveRecording(w, r, "credit_card", "find", http.StatusOK); err != nil {
+			panic(err)
+		}
 	})
 	defer server.Close()
 
@@ -370,24 +261,28 @@ func TestFindCreditCard(t *testing.T) {
 
 	g := gw.CreditCard()
 
-	card, err := g.Find("ghd3x6")
+	card, err := g.Find(testCard.Token)
 
 	t.Log(card)
 
 	if err != nil {
 		t.Fatal(err)
 	}
-	if card.Token != "ghd3x6" {
+	if card.Token != testCard.Token {
 		t.Fatal("tokens do not match")
 	}
+	if card.CardType != "Visa" {
+		t.Fatal(fmt.Sprintf("CardType should be %q, got %q", card.CardType, "Visa"))
+	}
+
+	_ = testCustomerDelete(customer.Id)
 }
 
-func TestFindCreditCardBadData(t *testing.T) {
-	var response = []byte(``)
-
+func TestFindCreditCard404(t *testing.T) {
 	server := newServer(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusNotFound)
-		writeZip(w, response)
+		if err := serveRecording(w, r, "credit_card", "find_404", http.StatusNotFound); err != nil {
+			panic(err)
+		}
 	})
 	defer server.Close()
 
@@ -405,57 +300,14 @@ func TestFindCreditCardBadData(t *testing.T) {
 }
 
 func TestSaveCreditCardWithVenmoSDKPaymentMethodCode(t *testing.T) {
-	var response = []byte(`<?xml version="1.0" encoding="UTF-8"?>
-<credit-card>
-  <billing-address>
-    <id>np</id>
-    <customer-id>20414939</customer-id>
-    <first-name nil="true"/>
-    <last-name nil="true"/>
-    <company nil="true"/>
-    <street-address nil="true"/>
-    <extended-address nil="true"/>
-    <locality nil="true"/>
-    <region nil="true"/>
-    <postal-code>60614</postal-code>
-    <country-code-alpha2 nil="true"/>
-    <country-code-alpha3 nil="true"/>
-    <country-code-numeric nil="true"/>
-    <country-name nil="true"/>
-    <created-at type="datetime">2013-11-20T23:05:21Z</created-at>
-    <updated-at type="datetime">2013-11-20T23:05:21Z</updated-at>
-  </billing-address>
-  <bin>411111</bin>
-  <card-type>Visa</card-type>
-  <cardholder-name nil="true"/>
-  <commercial>Unknown</commercial>
-  <country-of-issuance>Unknown</country-of-issuance>
-  <created-at type="datetime">2013-11-20T23:05:21Z</created-at>
-  <customer-id>20414939</customer-id>
-  <customer-location>US</customer-location>
-  <debit>Unknown</debit>
-  <default type="boolean">true</default>
-  <durbin-regulated>Unknown</durbin-regulated>
-  <expiration-month>01</expiration-month>
-  <expiration-year>2015</expiration-year>
-  <expired type="boolean">false</expired>
-  <healthcare>Unknown</healthcare>
-  <image-url>https://assets.braintreegateway.com/payment_method_logo/visa_via_venmo.png?environment=sandbox&amp;merchant_id=foo</image-url>
-  <issuing-bank>Unknown</issuing-bank>
-  <last-4>1111</last-4>
-  <payroll>Unknown</payroll>
-  <prepaid>Unknown</prepaid>
-  <subscriptions type="array"/>
-  <token>5qqpbm</token>
-  <unique-number-identifier>0d4987f6852ffa8612463aff627d84b5</unique-number-identifier>
-  <updated-at type="datetime">2013-11-20T23:05:21Z</updated-at>
-  <venmo-sdk type="boolean">true</venmo-sdk>
-  <verifications type="array"/>
-</credit-card>`)
+	customer := testCustomer
+	customer.Id = "cc_w_venmo_sdk_code"
+	_ = testCustomerCreate(&customer)
 
 	server := newServer(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusCreated)
-		writeZip(w, response)
+		if err := serveRecording(w, r, "credit_card", "create_with_vemo_sdk_code", http.StatusOK); err != nil {
+			panic(err)
+		}
 	})
 	defer server.Close()
 
@@ -464,7 +316,7 @@ func TestSaveCreditCardWithVenmoSDKPaymentMethodCode(t *testing.T) {
 	g := gw.CreditCard()
 
 	card := CreditCard{
-		CustomerId:                "20414939",
+		CustomerId:                customer.Id,
 		VenmoSDKPaymentMethodCode: "stub-" + testCreditCards["visa"].Number,
 	}
 	err := g.Create(&card)
@@ -474,42 +326,18 @@ func TestSaveCreditCardWithVenmoSDKPaymentMethodCode(t *testing.T) {
 	if !card.VenmoSDK {
 		t.Fatal("venmo card not marked")
 	}
+	_ = testCustomerDelete(customer.Id)
 }
 
 func TestSaveCreditCardWithVenmoSDKSession(t *testing.T) {
-	var response = []byte(`<?xml version="1.0" encoding="UTF-8"?>
-<credit-card>
-  <bin>411111</bin>
-  <card-type>Visa</card-type>
-  <cardholder-name nil="true"/>
-  <commercial>Unknown</commercial>
-  <country-of-issuance>Unknown</country-of-issuance>
-  <created-at type="datetime">2013-11-20T23:05:22Z</created-at>
-  <customer-id>81995216</customer-id>
-  <customer-location>US</customer-location>
-  <debit>Unknown</debit>
-  <default type="boolean">true</default>
-  <durbin-regulated>Unknown</durbin-regulated>
-  <expiration-month>05</expiration-month>
-  <expiration-year>2014</expiration-year>
-  <expired type="boolean">false</expired>
-  <healthcare>Unknown</healthcare>
-  <image-url>https://assets.braintreegateway.com/payment_method_logo/visa_via_venmo.png?environment=sandbox&amp;merchant_id=foo</image-url>
-  <issuing-bank>Unknown</issuing-bank>
-  <last-4>1111</last-4>
-  <payroll>Unknown</payroll>
-  <prepaid>Unknown</prepaid>
-  <subscriptions type="array"/>
-  <token>fj3tdw</token>
-  <unique-number-identifier>0d4987f6852ffa8612463aff627d84b5</unique-number-identifier>
-  <updated-at type="datetime">2013-11-20T23:05:22Z</updated-at>
-  <venmo-sdk type="boolean">true</venmo-sdk>
-  <verifications type="array"/>
-</credit-card>`)
+	customer := testCustomer
+	customer.Id = "cc_w_venmo_sdk_sesson"
+	_ = testCustomerCreate(&customer)
 
 	server := newServer(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusCreated)
-		writeZip(w, response)
+		if err := serveRecording(w, r, "credit_card", "create_with_vemo_sdk_session", http.StatusOK); err != nil {
+			panic(err)
+		}
 	})
 	defer server.Close()
 
@@ -518,7 +346,7 @@ func TestSaveCreditCardWithVenmoSDKSession(t *testing.T) {
 	g := gw.CreditCard()
 
 	card := CreditCard{
-		CustomerId:     "81995216",
+		CustomerId:     customer.Id,
 		Number:         testCreditCards["visa"].Number,
 		ExpirationDate: "05/14",
 		Options: &CreditCardOptions{
@@ -532,4 +360,6 @@ func TestSaveCreditCardWithVenmoSDKSession(t *testing.T) {
 	if !card.VenmoSDK {
 		t.Fatal("venmo card not marked")
 	}
+
+	_ = testCustomerDelete(customer.Id)
 }
