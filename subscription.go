ba10058c53baeb5113bf197f7828df6608e6b978
Cory LaNou <cory@joinlocal.com>
2013-12-02 10:01:55 -0700

    more subscription discounts

--- subscription.go
+++ subscription.go
@@ -12,39 +12,54 @@ const (
 type (
 	Subscriptions []Subscription
 	Subscription  struct {
-		XMLName                 string               `xml:"subscription"`
-		Id                      string               `xml:"id,omitempty"`
-		Balance                 float64              `xml:"balance,omitempty"`
-		BillingDayOfMonth       string               `xml:"billing-day-of-month,omitempty"`
-		BillingPeriodEndDate    string               `xml:"billing-period-end-date,omitempty"`
-		BillingPeriodStartDate  string               `xml:"billing-period-start-date,omitempty"`
-		CurrentBillingCycle     string               `xml:"current-billing-cycle,omitempty"`
-		DaysPastDue             string               `xml:"days-past-due,omitempty"`
-		Customer                *Customer            `xml:"customer,omitempty"`
-		CreditCard              *CreditCard          `xml:"credit-card,omitempty"`
-		Discounts               *Discounts           `xml:"discounts,omitempty"`
-		FailureCount            string               `xml:"failure-count,omitempty"`
-		FirstBillingDate        string               `xml:"first-billing-date,omitempty"`
-		MerchantAccountId       string               `xml:"merchant-account-id,omitempty"`
-		NeverExpires            string               `xml:"never-expires,omitempty"` // bool
-		NextBillAmount          float64              `xml:"next-bill-amount,omitempty"`
-		NextBillingPeriodAmount float64              `xml:"next-billing-period-amount,omitempty"`
-		NextBillingDate         string               `xml:"next-billing-date,omitempty"`
-		NumberOfBillingCycles   string               `xml:"number-of-billing-cycles,omitempty"` // int
-		PaidThroughDate         string               `xml:"paid-through-date,omitempty"`
-		PaymentMethodToken      string               `xml:"payment-method-token,omitempty"`
-		PlanId                  string               `xml:"plan-id,omitempty"`
-		Price                   float64              `xml:"price,omitempty"`
-		Status                  string               `xml:"status,omitempty"`
-		TrialDuration           string               `xml:"trial-duration,omitempty"`
-		TrialDurationUnit       string               `xml:"trial-duration-unit,omitempty"`
-		TrialPeriod             string               `xml:"trial-period,omitempty"` // bool
-		Transactions            *Transactions        `xml:"transactions,omitempty"`
-		Options                 *SubscriptionOptions `xml:"options,omitempty"`
+		Id                     string  `xml:"id,omitempty"`
+		Balance                float64 `xml:"balance,omitempty"`
+		BillingDayOfMonth      string  `xml:"billing-day-of-month,omitempty"`
+		BillingPeriodEndDate   string  `xml:"billing-period-end-date,omitempty"`
+		BillingPeriodStartDate string  `xml:"billing-period-start-date,omitempty"`
+		CurrentBillingCycle    string  `xml:"current-billing-cycle,omitempty"`
+		DaysPastDue            string  `xml:"days-past-due,omitempty"`
+		//Customer                *Customer            `xml:"customer,omitempty"`
+		SubscriptionDiscounts   *SubscriptionDiscountObject `xml:"discounts,omitempty"`
+		FailureCount            string                      `xml:"failure-count,omitempty"`
+		FirstBillingDate        string                      `xml:"first-billing-date,omitempty"`
+		MerchantAccountId       string                      `xml:"merchant-account-id,omitempty"`
+		NeverExpires            string                      `xml:"never-expires,omitempty"` // bool
+		NextBillAmount          float64                     `xml:"next-bill-amount,omitempty"`
+		NextBillingPeriodAmount float64                     `xml:"next-billing-period-amount,omitempty"`
+		NextBillingDate         string                      `xml:"next-billing-date,omitempty"`
+		NumberOfBillingCycles   string                      `xml:"number-of-billing-cycles,omitempty"` // int
+		PaidThroughDate         string                      `xml:"paid-through-date,omitempty"`
+		PaymentMethodToken      string                      `xml:"payment-method-token,omitempty"`
+		PlanId                  string                      `xml:"plan-id,omitempty"`
+		Price                   float64                     `xml:"price,omitempty"`
+		Status                  string                      `xml:"status,omitempty"`
+		TrialDuration           string                      `xml:"trial-duration,omitempty"`
+		TrialDurationUnit       string                      `xml:"trial-duration-unit,omitempty"`
+		TrialPeriod             string                      `xml:"trial-period,omitempty"` // bool
+		Transactions            *Transactions               `xml:"transactions,omitempty"`
+		Options                 *SubscriptionOptions        `xml:"options,omitempty"`
 		// AddOns                  []interface{} `xml:"add-ons,omitempty"`
 		// Descriptor              interface{}   `xml:"descriptor,omitempty"`   // struct with name, phone
+		ApiErrors
 	}
 
+	SubscriptionDiscountObject struct {
+		Add *SubscriptionDiscountItems `xml:"add,omitempty"`
+	}
+
+	SubscriptionDiscountItems []SubscriptionDiscountItem
+	SubscriptionDiscountItem  struct {
+		Item *SubscriptionDiscounts `xml:"item,omitempty"`
+		Type string                 `xml:"type,attr,omitempty"`
+	}
+
+	SubscriptionDiscounts []SubscriptionDiscount
+	SubscriptionDiscount  struct {
+		Id            string  `xml:"inherited-from-id,omitempty"`
+		Amount        float64 `xml:"amount,omitempty"`
+		BillingCycles string  `xml:"number-of-billing-cycles,omitempty"`
+	}
 	SubscriptionList struct {
 		XMLName       string        `xml:"subscriptions"`
 		Subscriptions Subscriptions `xml:"subscription"`
--- subscription_test.go
+++ subscription_test.go
@@ -5,175 +5,78 @@ import (
 	"testing"
 )
 
-func TestSubscriptionCreate(t *testing.T) {
-	var response = []byte(`<?xml version="1.0" encoding="UTF-8"?>
-<subscription>
-  <add-ons type="array"/>
-  <balance>0.00</balance>
-  <billing-day-of-month type="integer">20</billing-day-of-month>
-  <billing-period-end-date type="date">2013-12-19</billing-period-end-date>
-  <billing-period-start-date type="date">2013-11-20</billing-period-start-date>
-  <current-billing-cycle type="integer">1</current-billing-cycle>
-  <days-past-due nil="true"/>
-  <discounts type="array"/>
-  <failure-count type="integer">0</failure-count>
-  <first-billing-date type="date">2013-11-20</first-billing-date>
-  <id>4j2ntb</id>
-  <merchant-account-id>foo</merchant-account-id>
-  <never-expires type="boolean">false</never-expires>
-  <next-bill-amount>10.00</next-bill-amount>
-  <next-billing-period-amount>10.00</next-billing-period-amount>
-  <next-billing-date type="date">2013-12-20</next-billing-date>
-  <number-of-billing-cycles type="integer">2</number-of-billing-cycles>
-  <paid-through-date type="date">2013-12-19</paid-through-date>
-  <payment-method-token>fzqy62</payment-method-token>
-  <plan-id>test_plan</plan-id>
-  <price>10.00</price>
-  <status>Active</status>
-  <trial-duration nil="true"/>
-  <trial-duration-unit nil="true"/>
-  <trial-period type="boolean">false</trial-period>
-  <descriptor>
-    <name nil="true"/>
-    <phone nil="true"/>
-  </descriptor>
-  <transactions type="array">
-    <transaction>
-      <id>3fg66b</id>
-      <status>submitted_for_settlement</status>
-      <type>sale</type>
-      <currency-iso-code>USD</currency-iso-code>
-      <amount>10.00</amount>
-      <merchant-account-id>foo</merchant-account-id>
-      <order-id nil="true"/>
-      <created-at type="datetime">2013-11-20T23:05:32Z</created-at>
-      <updated-at type="datetime">2013-11-20T23:05:33Z</updated-at>
-      <customer>
-        <id>17235299</id>
-        <first-name>Lionel</first-name>
-        <last-name>Barrow</last-name>
-        <company>Braintree</company>
-        <email>lionel.barrow@example.com</email>
-        <website>http://www.example.com</website>
-        <phone>312.555.1234</phone>
-        <fax>614.555.5678</fax>
-      </customer>
-      <billing>
-        <id nil="true"/>
-        <first-name nil="true"/>
-        <last-name nil="true"/>
-        <company nil="true"/>
-        <street-address nil="true"/>
-        <extended-address nil="true"/>
-        <locality nil="true"/>
-        <region nil="true"/>
-        <postal-code nil="true"/>
-        <country-name nil="true"/>
-        <country-code-alpha2 nil="true"/>
-        <country-code-alpha3 nil="true"/>
-        <country-code-numeric nil="true"/>
-      </billing>
-      <refund-id nil="true"/>
-      <refund-ids type="array"/>
-      <refunded-transaction-id nil="true"/>
-      <settlement-batch-id nil="true"/>
-      <shipping>
-        <id nil="true"/>
-        <first-name nil="true"/>
-        <last-name nil="true"/>
-        <company nil="true"/>
-        <street-address nil="true"/>
-        <extended-address nil="true"/>
-        <locality nil="true"/>
-        <region nil="true"/>
-        <postal-code nil="true"/>
-        <country-name nil="true"/>
-        <country-code-alpha2 nil="true"/>
-        <country-code-alpha3 nil="true"/>
-        <country-code-numeric nil="true"/>
-      </shipping>
-      <custom-fields>
-      </custom-fields>
-      <avs-error-response-code nil="true"/>
-      <avs-postal-code-response-code>I</avs-postal-code-response-code>
-      <avs-street-address-response-code>I</avs-street-address-response-code>
-      <cvv-response-code>I</cvv-response-code>
-      <gateway-rejection-reason nil="true"/>
-      <processor-authorization-code>BF91G9</processor-authorization-code>
-      <processor-response-code>1000</processor-response-code>
-      <processor-response-text>Approved</processor-response-text>
-      <voice-referral-number nil="true"/>
-      <purchase-order-number nil="true"/>
-      <tax-amount nil="true"/>
-      <tax-exempt type="boolean">false</tax-exempt>
-      <credit-card>
-        <token>fzqy62</token>
-        <bin>411111</bin>
-        <last-4>1111</last-4>
-        <card-type>Visa</card-type>
-        <expiration-month>05</expiration-month>
-        <expiration-year>2014</expiration-year>
-        <customer-location>US</customer-location>
-        <cardholder-name nil="true"/>
-        <image-url>https://assets.braintreegateway.com/payment_method_logo/visa.png?environment=sandbox&amp;merchant_id=foo</image-url>
-        <prepaid>Unknown</prepaid>
-        <healthcare>Unknown</healthcare>
-        <debit>Unknown</debit>
-        <durbin-regulated>Unknown</durbin-regulated>
-        <commercial>Unknown</commercial>
-        <payroll>Unknown</payroll>
-        <issuing-bank>Unknown</issuing-bank>
-        <country-of-issuance>Unknown</country-of-issuance>
-        <venmo-sdk type="boolean">false</venmo-sdk>
-      </credit-card>
-      <status-history type="array">
-        <status-event>
-          <timestamp type="datetime">2013-11-20T23:05:33Z</timestamp>
-          <status>authorized</status>
-          <amount>10.00</amount>
-          <user>corylanou-sandbox</user>
-          <transaction-source>Recurring</transaction-source>
-        </status-event>
-        <status-event>
-          <timestamp type="datetime">2013-11-20T23:05:33Z</timestamp>
-          <status>submitted_for_settlement</status>
-          <amount>10.00</amount>
-          <user>corylanou-sandbox</user>
-          <transaction-source>Recurring</transaction-source>
-        </status-event>
-      </status-history>
-      <plan-id>test_plan</plan-id>
-      <subscription-id>4j2ntb</subscription-id>
-      <subscription>
-        <billing-period-end-date type="date">2013-12-19</billing-period-end-date>
-        <billing-period-start-date type="date">2013-11-20</billing-period-start-date>
-      </subscription>
-      <add-ons type="array"/>
-      <discounts type="array"/>
-      <descriptor>
-        <name nil="true"/>
-        <phone nil="true"/>
-      </descriptor>
-      <recurring type="boolean">true</recurring>
-      <channel nil="true"/>
-      <service-fee-amount nil="true"/>
-      <escrow-status nil="true"/>
-      <disbursement-details>
-        <disbursement-date nil="true"/>
-        <settlement-amount nil="true"/>
-        <settlement-currency-iso-code nil="true"/>
-        <settlement-currency-exchange-rate nil="true"/>
-        <funds-held nil="true"/>
-      </disbursement-details>
-    </transaction>
-  </transactions>
-</subscription>`)
+// Everything you wanted to know about subscriptions:
+// https://www.braintreepayments.com/docs/ruby/subscriptions/create
 
+func TestSubscriptionCreateWithDiscount(t *testing.T) {
 	server := newServer(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusCreated)
-		writeZip(w, response)
+		if err := serveRecording(w, r, "subscription", "create_with_discount", http.StatusCreated); err != nil {
+			panic(err)
+		}
 	})
 	defer server.Close()
+
+	customer := testCustomer
+	if c, err := testCustomerFindOrCreate(customer); err != nil {
+		t.Fatal(err, "Unable to set up test customer")
+	} else {
+		customer = c
+	}
+
+	creditCard := testCreditCard
+	creditCard.CustomerId = customer.Id
+	if err := testCreditCardCreate(&creditCard); err != nil {
+		t.Fatal(err, "Unable to set up test credit card")
+	}
+
+	d := SubscriptionDiscount{}
+	d.Id = "test_discount"
+	d.Amount = 5
+	d.BillingCycles = "1"
+	items := SubscriptionDiscountItems{{Item: &SubscriptionDiscounts{d}, Type: "array"}}
+	sub := Subscription{
+		PaymentMethodToken:    creditCard.Token,
+		PlanId:                "test_plan",
+		SubscriptionDiscounts: &SubscriptionDiscountObject{Add: &items},
+	}
+
+	gw := Braintree{BaseURL: server.URL}
+
+	err := gw.Subscription().Create(&sub)
+
+	t.Log(sub)
+
+	if err != nil {
+		t.Log(sub.ErrorMessage)
+		t.Fatal(err)
+	}
+
+	if !sub.Success() {
+		t.Fatalf("Recieved an error of %q", sub.ErrorMessage)
+	}
+
+}
+
+func TestSubscriptionCreate(t *testing.T) {
+	server := newServer(func(w http.ResponseWriter, r *http.Request) {
+		if err := serveRecording(w, r, "subscription", "create", http.StatusCreated); err != nil {
+			panic(err)
+		}
+	})
+	defer server.Close()
+
+	customer := testCustomer
+	if c, err := testCustomerFindOrCreate(customer); err != nil {
+		t.Fatal(err, "Unable to set up test customer")
+	} else {
+		customer = c
+	}
+
+	creditCard := testCreditCard
+	creditCard.CustomerId = customer.Id
+	if err := testCreditCardCreate(&creditCard); err != nil {
+		t.Fatal(err, "Unable to set up test credit card")
+	}
 
 	gw := Braintree{BaseURL: server.URL}
 
@@ -181,7 +84,7 @@ func TestSubscriptionCreate(t *testing.T) {
 
 	// Create
 	subscription := Subscription{
-		PaymentMethodToken: "fzqy62",
+		PaymentMethodToken: creditCard.Token,
 		PlanId:             "test_plan",
 		Options: &SubscriptionOptions{
 			ProrateCharges:                       true,
